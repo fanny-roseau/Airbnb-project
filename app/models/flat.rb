b084ded3a7dbd49ab2fae6ccbee1fe5a62af7f2e
Ake Anouar <a.akerrouach@gmail.com>
2015-04-21 16:37:51 +0200

    creation de flat model avec les validations

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,11 @@
 class Flat < ActiveRecord::Base
+  belongs_to :user
+
+  validate :property_type, presence: true,
+  validate :room_type, presence: true,
+  validate :capacity, presence: true,
+  validate :city, presence: true,
+  validate :price, presence: true,
+  validate :street, presence: true,
+  validate :zip_code, presence: true,
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421131554) do
+ActiveRecord::Schema.define(version: 20150421140954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flats", force: :cascade do |t|
+    t.string   "property_type"
+    t.string   "room_type"
+    t.integer  "capacity"
+    t.string   "city"
+    t.text     "presentation"
+    t.integer  "price"
+    t.string   "street"
+    t.integer  "zip_code"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
