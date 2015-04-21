bedcf6db6f3316ad43875539488ebf7a9ad3c632
Ake Anouar <a.akerrouach@gmail.com>
2015-04-21 18:19:17 +0200

    add flats controller and model

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,12 @@
 class Flat < ActiveRecord::Base
+  belongs_to :users
+
+  validate :property_type, presence: true, inclusion: {in:["appartement", "maison"]}
+  validate :room_type, presence: true, inclusion: {in:["logement entier", "chambre privée", "chambre partagée"]}
+  validate :capacity, presence: true,
+  validate :city, presence: true,
+  validate :zip_code, presence: true,
+  validate :street, presence: true,
+  validate :price,presence: true,
+
 end
