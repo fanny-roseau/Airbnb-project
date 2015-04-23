03649fa99e3fd21c70322dd0e99248487b5db58b
Kevin Buhler <buhler.kevin@ymail.com>
2015-04-23 14:54:32 +0200

    add flat users to my profil

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,12 +1,18 @@
 class Flat < ActiveRecord::Base
   belongs_to :user
 
-  validates :property_type, presence: true, inclusion: {in:["appartement", "maison"]}
-  validates :room_type, presence: true, inclusion: {in:["logement entier", "chambre privée", "chambre partagée"]}
+  validates :property_type, presence: true, inclusion: {in:["Appartement", "Maison"]}
+  validates :room_type, presence: true, inclusion: {in:["Logement entier", "Chambre privée", "Chambre partagée"]}
   validates :capacity, presence: true
   validates :city, presence: true
   validates :zip_code, presence: true
   validates :street, presence: true
   validates :price,presence: true
+
+  has_attached_file :picture,
+    styles: { medium: "400x400>", thumb: "100x100>" }
+
+  validates_attachment_content_type :picture,
+    content_type: /\Aimage\/.*\z/
 
 end
